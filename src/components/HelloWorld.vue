a9aca7cfea8b55cb173930987fe540c0101ada51
sergey.puzanov <sergey.puzanov@red-soft.ru>
2018-05-16 12:41:02 +0300

    git init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+.hello
+  .hello-title {{ msg }}
+
+  h4 vm-radio
+  //- vm-button asd
+  //- vm-radio(marker="1" :value="test" @input="testFunc" label="first")
+  //- vm-radio(marker="2" :value="test" @input="testFunc" label="second")
+  h4 asdasd
+  test-radio(marker="1" :value="test" @input="testFunc" label="first")
+  test-radio(marker="2" :value="test" @input="testFunc" label="second")
+
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  msg: string = 'Vue';
+  test: string = '1';
+  test2: string = '2';
+
+  testFunc(tt) {
+    console.log('asdas', tt)
+    this.test = tt;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="postcss">
+.hello {
+
+  &-title {
+    background: black;
+  }
+}
+
+/* .hello-title {
+  background: yellow;
+} */
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TestRadio.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+    .vm-radio(
+        @click="click"
+    ) asfsdafsadf
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component({
+    name: 'test-radio',
+    // model: {
+    //     prop: 'model',
+    //     event: 'input'
+    // }
+})
+export default class TestRadio extends Vue {
+
+    @Prop() value: any;
+    @Prop({ required: true }) marker: any;
+
+    public click() {
+        console.log('click', this.marker);
+        if (this.value !== this.marker) {
+            console.log('CHANGE')
+            this.$emit('input', this.marker);
+        }
+    }
+}
+</script>
